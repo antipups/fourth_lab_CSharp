6155d6939fba4760eaf2c04df4a19398743e5320
antipups <nick.kurkurin@protonmail.com>
2020-04-05 13:49:50 +0300

    Лаба сделана и сдана. Причем с логаами.

---
+++ fourth_lab/Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿using System.Windows.Forms;
+
+namespace fourth_lab
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.AcceptsReturn = true;
+            this.textBox1.Location = new System.Drawing.Point(30, 36);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(378, 27);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "Введите путь";
+            this.textBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyDown);
+            // 
+            // treeView1
+            // 
+            this.treeView1.Location = new System.Drawing.Point(30, 80);
+            this.treeView1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(377, 428);
+            this.treeView1.TabIndex = 3;
+            this.treeView1.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeView1_AfterSelect);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(414, 36);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(383, 27);
+            this.textBox2.TabIndex = 4;
+            this.textBox2.Text = "Введите слова которые хотите видеть в .rtf";
+            this.textBox2.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox2_KeyDown);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(413, 82);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.Size = new System.Drawing.Size(365, 352);
+            this.textBox3.TabIndex = 5;
+            this.textBox3.Text = "Логи";
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 562);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.treeView1);
+            this.Controls.Add(this.textBox1);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+    }
+}
---
+++ fourth_lab/Form1.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Windows.Forms;
+
+namespace fourth_lab
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            treeView1.Visible = false;
+            // заполняем дерево дисками
+            
+        }
+		// событие перед раскрытием узла
+        void treeView1_BeforeExpand(object sender, TreeViewCancelEventArgs e)
+        {
+            e.Node.Nodes.Clear();
+            string[] dirs;
+            try
+            {
+                if (Directory.Exists(e.Node.FullPath))
+                {
+                    dirs = Directory.GetDirectories(e.Node.FullPath);
+                    if (dirs.Length != 0)
+                    {
+                        for (int i = 0; i < dirs.Length; i++)
+                        {
+                            TreeNode dirNode = new TreeNode(new DirectoryInfo(dirs[i]).Name);
+                            FillTreeNode(dirNode, dirs[i]);
+                            e.Node.Nodes.Add(dirNode);
+                        }
+                    }
+                }
+            }
+            catch (Exception ex) { }
+        }
+		// событие перед выделением узла
+        void treeView1_BeforeSelect(object sender, TreeViewCancelEventArgs e)
+        {
+            e.Node.Nodes.Clear();
+            string[] dirs;
+            try
+            {
+                if(Directory.Exists(e.Node.FullPath))
+                {
+                    dirs = Directory.GetDirectories(e.Node.FullPath);
+                    if (dirs.Length!= 0)
+                    {
+                        for(int i=0; i<dirs.Length;i++)
+                        {
+                            TreeNode dirNode = new TreeNode(new DirectoryInfo(dirs[i]).Name);
+                            FillTreeNode(dirNode, dirs[i]);
+                            e.Node.Nodes.Add(dirNode);
+                        }
+                    }
+                } 
+            }
+            catch (Exception ex) { }
+        }
+		
+		// получаем все диски на компьютере
+        private void FillDriveNodes(String path)
+        {
+            try
+            {
+                foreach(string driveNode in Directory.GetDirectories(path))
+                {
+                    TreeNode treeNode = new TreeNode {Text = driveNode};
+                    // treeNode.Text = driveNode.Remove(0, driveNode.LastIndexOf("\\") + 1);
+                    FillTreeNode(treeNode, driveNode);
+                    treeView1.Nodes.Add(treeNode);
+                }
+            }
+            catch (Exception ex) { }
+        }
+		// получаем дочерние узлы для определенного узла
+        private void FillTreeNode(TreeNode driveNode, string path)
+        {
+            try
+            {
+                string[] dirs = Directory.GetDirectories(path);
+                foreach (string dir in dirs)
+                {
+                    TreeNode dirNode = new TreeNode();
+                    dirNode.Text = dir.Remove(0, dir.LastIndexOf("\\") + 1);
+                    driveNode.Nodes.Add(dirNode);
+                }
+            }
+            catch (Exception ex) { }
+        }
+
+        private void textBox1_KeyDown(object sender, KeyEventArgs e)
+            // this.textBox1.KeyDown += new KeyEventHandler(this.textBox1_KeyDown);
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                String path =  textBox1.Text;
+                if (!Directory.Exists(path))
+                {
+                    textBox1.Text = "ВВЕДИТЕ КОРРЕКТНЫЙ ПУТЬ";
+                }
+                else
+                {
+                    treeView1.Nodes.Clear();
+                    treeView1.Visible = true;
+                    treeView1.BeforeSelect += treeView1_BeforeSelect;
+                    treeView1.BeforeExpand += treeView1_BeforeExpand;
+                    textBox1.Clear();
+                    FillDriveNodes(path);
+                }
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            // throw new System.NotImplementedException();
+        }
+
+        private void textBox2_KeyDown(object sender, EventArgs e)
+            // this.textBox2.KeyDown += new KeyEventHandler(this.textBox2_KeyDown);
+        {
+            // throw new System.NotImplementedException();
+        }
+
+        private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            TreeNode CurrentNode = treeView1.SelectedNode;
+            String path_to_move;
+            try
+            {
+                path_to_move = get_path(CurrentNode).Substring(1) + "\\" + CurrentNode.Text;
+            }
+            catch (Exception exception)    // если нет родителя (if -- не для нас!)
+            {
+                path_to_move = treeView1.SelectedNode.Text;
+            }
+
+            textBox1.Text = path_to_move;
+            
+            // далее махинации с считыванием файла) (строка ниже - путь к rtf)
+            String path = "C:\\Users\\kurku\\Documents\\C#Projects\\fourth_lab_C#\\fourth_lab\\fourth_lab\\rtfs";
+            int amount_of_file = 0;
+            textBox3.Text = "";
+            foreach (String name_of_rtf in Directory.GetFiles(path))
+            {
+                RichTextBox RTB = new RichTextBox();
+                RTB.Rtf = File.ReadAllText(name_of_rtf);
+                textBox3.Text += "Открыт файл -- " + name_of_rtf + "\n";
+                if (RTB.Text.IndexOf(textBox2.Text, StringComparison.Ordinal) >= 0)
+                {
+                    textBox3.Text += "В файле найдено совпадение -- " + textBox2.Text +  "\n";
+                    File.Move(name_of_rtf, path_to_move + "\\" + name_of_rtf.Substring(name_of_rtf.LastIndexOf("\\", StringComparison.Ordinal) + 1));
+                    amount_of_file += 1;
+                }
+            }
+
+            textBox2.Text = "Перемещено - " + amount_of_file.ToString() + " файла.";
+        }
+
+        private string get_path(TreeNode CurrentNode)
+        {
+            if (CurrentNode.Parent == null)
+            {
+                return "";
+            }
+            else
+            {
+                String parent = get_path(CurrentNode.Parent);
+                return parent + "\\" + CurrentNode.Parent.Text;
+            }
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void textBox3_TextChanged_1(object sender, EventArgs e)
+        {
+            // throw new System.NotImplementedException();
+        }
+    }
+}
